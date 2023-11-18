94e50920febf096cb1b944b303a49507154c3d11
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-18 10:35:11 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,11 @@
+public class Function {
+    public static int add(int a,int b){
+        return a+b;
+    }
+    public static void main(String[] args) {
+        int a = 45;
+        int b = 16;
+        int result = add(a,b);
+        System.out.println("Result is:"+result);
+    }
+}
