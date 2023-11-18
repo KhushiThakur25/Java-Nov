212b31b417e42f3954a7bf35ccca36f8a3acf059
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-11-18 10:16:54 +0530

    Add files via upload

---
+++ ForEach.java
@@ -0,0 +1,11 @@
+public class ForEach {
+    public static void main(String[] args) {
+        //for(data_type variable : array_name){
+        //   ....}
+            int arr[] = {23,45,6,8,9};
+            for(int i:arr){
+                System.out.println(i);
+            }
+        }
+    }
+
