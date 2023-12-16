f713507171ae715e1aed8fbc3fcca797a1a9787c
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-16 11:03:08 +0530

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,17 +1,26 @@
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Frame {
-    public Frame(){
+    public Frame() {
         JFrame jFrame = new JFrame();
         JButton jButton = new JButton("click me");
-        jButton.setBounds(100,40,100,30);
+        JLabel jLabel = new JLabel("this is button..");
+        JTextField jTextField  = new JTextField();
+        jTextField.setBounds(100,150,250,30);
+        jLabel.setBounds(120, 90, 250, 30);
+        jButton.setBounds(100, 40, 100, 30);
         jFrame.add(jButton);
+        jFrame.add(jLabel);
+        jFrame.add(jTextField);
         jFrame.setTitle("This is my frame..");
         jFrame.setLayout(null);
-        jFrame.setSize(400, 400);
+        jFrame.setBounds(200, 200, 400, 400);
         jFrame.setVisible(true);
     }
+
     public static void main(String[] args) {
         Frame f = new Frame();
     }
