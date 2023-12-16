e7166cb9cc1920f0e51acedeeb3a0d0df711830d
KhushiThakur25 <113238209+KhushiThakur25@users.noreply.github.com>
2023-12-16 10:45:01 +0530

    Add files via upload

--- Frame.java
+++ Frame.java
@@ -1,11 +1,16 @@
+import javax.swing.JButton;
 import javax.swing.JFrame;
 
 public class Frame {
     public Frame(){
         JFrame jFrame = new JFrame();
+        JButton jButton = new JButton("click me");
+        jButton.setBounds(100,40,100,30);
+        jFrame.add(jButton);
+        jFrame.setTitle("This is my frame..");
         jFrame.setLayout(null);
         jFrame.setSize(400, 400);
-        jFrame.setVisible(false);
+        jFrame.setVisible(true);
     }
     public static void main(String[] args) {
         Frame f = new Frame();
